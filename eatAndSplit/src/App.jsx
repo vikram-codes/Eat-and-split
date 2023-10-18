1c1f21922e8c972f69171f97fa5d410120639e98
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-18 20:27:59 +0530

    solved the bug that was making the FormSplitBill form not render with headline

--- eatAndSplit/src/App.jsx
+++ eatAndSplit/src/App.jsx
@@ -3,14 +3,16 @@ import FriendList from "./components/FriendList";
 import FormAddFriend from "./components/FormAddFriend";
 import Button from "./components/Button";
 import FormSplitBill from "./components/FormSplitBill";
+import { useState } from "react";
 
 export default function App() {
+  const [addFriend, setAddFriend] = useState(false);
   return (
     <>
       <div className="app">
         <div className="sidebar">
           <FriendList />
-          <FormAddFriend />
+          {addFriend && <FormAddFriend />}
           <Button>Add friend</Button>
         </div>
         <FormSplitBill />
